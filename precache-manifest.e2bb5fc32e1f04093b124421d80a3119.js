a5e81eee6f8749ab3adf576b28c8da7f58c6ee19
imkrunalkanojiya <kanojiyakrunal5@gmail.com>
2023-01-09 10:17:47 +0530

    Updates

---
+++ precache-manifest.e2bb5fc32e1f04093b124421d80a3119.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80724c76915033e390e362b130e6af2d",
+    "url": "/react-masonry-view/index.html"
+  },
+  {
+    "revision": "e686604b54a56f5feaaf",
+    "url": "/react-masonry-view/static/css/2.d6bb9b3c.chunk.css"
+  },
+  {
+    "revision": "c4fc2c943cf9038b9356",
+    "url": "/react-masonry-view/static/css/main.9a195b2c.chunk.css"
+  },
+  {
+    "revision": "e686604b54a56f5feaaf",
+    "url": "/react-masonry-view/static/js/2.7f03cff1.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-masonry-view/static/js/2.7f03cff1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4fc2c943cf9038b9356",
+    "url": "/react-masonry-view/static/js/main.3f7e85d7.chunk.js"
+  },
+  {
+    "revision": "21a9e90e67201549b394",
+    "url": "/react-masonry-view/static/js/runtime-main.28d2d269.js"
+  },
+  {
+    "revision": "14458513c47673e373a8e13837f09345",
+    "url": "/react-masonry-view/static/media/img1.14458513.jpg"
+  },
+  {
+    "revision": "e680f0c4ec01804874f34e89c7fd5da9",
+    "url": "/react-masonry-view/static/media/img2.e680f0c4.jpg"
+  },
+  {
+    "revision": "105054300ae7442d963ead1bd7deebbb",
+    "url": "/react-masonry-view/static/media/img3.10505430.jpg"
+  },
+  {
+    "revision": "90cdb7ebd5565c3e6f9210628da20a83",
+    "url": "/react-masonry-view/static/media/img4.90cdb7eb.jpg"
+  },
+  {
+    "revision": "e414fe003d43ea48dcf626fd9c9277db",
+    "url": "/react-masonry-view/static/media/img5.e414fe00.jpg"
+  },
+  {
+    "revision": "f050139129a864cd1d18b3f409c642ad",
+    "url": "/react-masonry-view/static/media/img6.f0501391.jpg"
+  },
+  {
+    "revision": "88ed5c839e7be3f56523a6913505c8c2",
+    "url": "/react-masonry-view/static/media/img7.88ed5c83.jpg"
+  },
+  {
+    "revision": "91d34203f4ab3f3be26a335c9b58668e",
+    "url": "/react-masonry-view/static/media/img8.91d34203.jpg"
+  },
+  {
+    "revision": "b6c1a559661f10737dd22e64f596bca2",
+    "url": "/react-masonry-view/static/media/img9.b6c1a559.jpg"
+  }
+]);
